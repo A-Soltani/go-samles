e6178f945a2fe8dc38581452c5d6b7b9be112de9
Ali Soltani <soltani1384@gmail.com>
2020-10-03 08:16:47 +0330

    using slice in set deck

--- cards/main.go
+++ cards/main.go
@@ -10,10 +10,22 @@ func main() {
 	card := "Ace of Spades"
 	fiveSpadesCard := getSpadesCard(5)
 
-	fmt.Print(card, "\n")
-	fmt.Print(fiveSpadesCard)
+	fmt.Println(card)
+	fmt.Println(fiveSpadesCard)
+
+	setDeck()
 }
 
 func getSpadesCard(number int) string {
 	return strconv.Itoa(number) + " of Spades"
+}
+
+func setDeck() {
+	cards := []string{"Ace of Diamonds"} // this is a slice
+	cards = append(cards, "6 of Spades")
+
+	fmt.Println("Deck cards:")
+	for i, card := range cards {
+		fmt.Println(i+1, card)
+	}
 }
